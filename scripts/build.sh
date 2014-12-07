8a12cf011604b87544fff6e3d8d426bd4f38cbe4
ZyX <kp-pav@yandex.ru>
2014-12-08 00:47:03 +0300

    Move merging to build.sh so that failing merge will fail build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,9 @@ while eval test -n "\"\$SCRIPT$i\"" ; do
 	eval \$SCRIPT$i
 	i=$(( i + 1 ))
 done
+
+cd deps
+git fetch origin master:origin-master
+git checkout origin-master
+git merge --no-ff --commit -m "Merge branch '${BRANCH_NAME}' into master" "${BRANCH_NAME}"
+git branch -f master
