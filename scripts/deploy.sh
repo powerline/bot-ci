588d76eb142e2fddbabd1145311776cecd22194c
ZyX <kp-pav@yandex.ru>
2014-12-07 19:45:39 +0300

    Rename origin/master to origin-master
    For some reason `origin/master` is considered ambigious (most likely I should 
    not’ve fetched to it explicitly).

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 cd deps
-git fetch origin master:origin/master
-git checkout origin/master
+git fetch origin master:origin-master
+git checkout origin-master
 git merge --no-ff --commit -m 'Merge' master
 git branch -f master
 git push origin master:master
