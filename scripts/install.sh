0946c8a6e55fd964d0e8a94f017b89797bfc308e
ZyX <kp-pav@yandex.ru>
2014-12-07 18:56:49 +0300

    Configure git

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,3 +5,6 @@ sudo apt-get install mercurial
 chmod 0600 keys/id_rsa
 cp keys/id_rsa ~/.ssh
 git clone ssh://git@github.com/powerline/deps
+
+git config --global user.name 'Travis CI bot'
+git config --global user.email 'bot@example.com'
