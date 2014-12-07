54ca381909b1e907fcee2622b9b6d7a2e4d3e5da
ZyX <kp-pav@yandex.ru>
2014-12-07 22:33:44 +0300

    Disable built-in make rules and variables

--- scripts/build-vim.sh
+++ scripts/build-vim.sh
@@ -29,7 +29,7 @@ else
 	CFGARGS="$CFGARGS --enable-pythoninterp=dynamic"
 fi
 ./configure $CFGARGS
-make
+make --no-builtin-rules --no-builtin-variables
 cp src/vim $ROOT/deps/vim/$SUBDIR/vim
 cd $ROOT/deps
 git add vim/$SUBDIR/vim
