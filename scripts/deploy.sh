7bb80e630a92e54b9172e0319553a889460daaf9
ZyX <kp-pav@yandex.ru>
2014-12-07 19:35:29 +0300

    Merge always, explicitly and with a message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 cd deps
-git pull origin master
+git fetch origin master:origin/master
+git checkout origin/master
+git merge --no-ff --commit -m 'Merge' master
+git branch -f master
 git push origin master:master
