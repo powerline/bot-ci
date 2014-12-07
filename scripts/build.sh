05699acf78a89cb079f77bca6b82787ce35be1ce
ZyX <kp-pav@yandex.ru>
2014-12-08 01:09:42 +0300

    Resolve merge conflicts automatically by using `recursive:theirs`

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,5 +10,5 @@ done
 cd deps
 git fetch origin master:origin-master
 git checkout origin-master
-git merge --no-ff --commit -m "Merge branch '${BRANCH_NAME}' into master" "${BRANCH_NAME}"
+git merge --strategy recursive --strategy-option theirs --no-ff --commit -m "Merge branch '${BRANCH_NAME}' into master" "${BRANCH_NAME}"
 git branch -f master
