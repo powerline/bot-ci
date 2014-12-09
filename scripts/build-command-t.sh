2e9b4e08dcaa8c20ff2241ff7b71cd569d3f236e
ZyX <kp-pav@yandex.ru>
2014-12-09 08:28:40 +0300

    Fix Command-T building script

--- scripts/build-command-t.sh
+++ scripts/build-command-t.sh
@@ -4,10 +4,10 @@
 mkdir -p deps/vim-plugins
 mkdir -p build/vim-plugins
 
+cd build/vim-plugins
 git clone --depth=1 https://github.com/wincent/Command-T command-t
 
-cd command-t
-cd ruby/command-t
+cd command-t/ruby/command-t
 ruby extconf.rb
 make
 
@@ -17,6 +17,8 @@ cd ..
 tar czvf $ROOT/deps/vim-plugins/command-t.tar.gz command-t
 mv .git-command-t command-t/.git
 
+cd $ROOT/deps
+git add vim-plugins/command-t.tar.gz
 git commit -m "Update Command-T
 
 git head:
