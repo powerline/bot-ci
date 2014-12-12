1f391eb9746554a341e2b05171d06e7ff417f2a1
ZyX <kp-pav@yandex.ru>
2014-12-12 23:19:18 +0300

    Also build libvterm

---
+++ scripts/build-libvterm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+. scripts/common/main.sh
+mkdir -p deps/libvterm
+mkdir -p build/
+bzr checkout --lightweight http://bazaar.leonerd.org.uk/c/libvterm/ build/libvterm
+cd build/libvterm
+make
+make test
+cp -L .libs/libvterm.so $ROOT/deps/libvterm
+cd $ROOT/deps
+git add libvterm/libvterm.so
+git commit libvterm -m "Add libvterm.so
+
+bzr head:
+
+$(cd $ROOT/build/libvterm && bzr log --limit=1 --show-ids| indent)"
