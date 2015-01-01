0f45f759063caee54092ed500d300a7cdb918d75
ZyX <kp-pav@yandex.ru>
2015-01-01 23:29:34 +0300

    Fix sed s/ separators

--- scripts/build-ucs2-cpython.sh
+++ scripts/build-ucs2-cpython.sh
@@ -14,7 +14,7 @@ sudo apt-get install -qq build-essential zlib1g-dev libreadline-dev \
                          libsqlite3-dev liblzma-dev libc6-dev tk-dev libdb-dev
 
 cd build/cpython-ucs2/$REV
-sed -r -i -e 's/^(\s*lib_dirs\s*=.*\[)/\1"/usr/lib/x86_64-linux-gnu",/' setup.py
+sed -r -i -e 's@^(\s*lib_dirs\s*=.*\[)@\1"/usr/lib/x86_64-linux-gnu",@' setup.py
 ./configure --enable-unicode=ucs2 --prefix=/opt/cpython-ucs2-$REV --enable-shared
 make
 sudo make install
