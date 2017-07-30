04e066ff90a5cdd713d670bc41f476f089e6bc90
ZyX <kp-pav@yandex.ru>
2017-07-31 01:58:10 +0300

    Omit building zpython if libpython was not found

--- scripts/build-zsh-and-zpython.sh
+++ scripts/build-zsh-and-zpython.sh
@@ -36,40 +36,53 @@ make install || true
 cd "$ZPYTHON_BDIR"
 mkdir build
 cd build
+BUILD_ZPYTHON=1
 LIBRARY_PATH="$(ldd "$PYTHON" | grep libpython | sed 's/^.* => //;s/ .*$//')"
 if test -z "$LIBRARY_PATH" ; then
 	ldd "$PYTHON"
 	PYTHON_PREFIX="$(env -i "$PYTHON" -c 'import sys ; print(sys.prefix)')"
 	if test -z "$PYTHON_PREFIX" ; then
-		exit 1
+		BUILD_ZPYTHON=0
+	else
+		for lib in "$PYTHON_PREFIX"/lib/libpython* ; do
+			if ! test -e "$lib" ; then
+				BUILD_ZPYTHON=0
+			fi
+			LIBRARY_PATH="$lib"
+		done
+		LIBRARY_DIR="$PYTHON_PREFIX/lib"
 	fi
-	for lib in "$PYTHON_PREFIX"/lib/libpython* ; do
-		if ! test -e "$lib" ; then
-			exit 1
-		fi
-		LIBRARY_PATH="$lib"
-	done
-	LIBRARY_DIR="$PYTHON_PREFIX/lib"
 else
 	LIBRARY_DIR="$(dirname "${LIBRARY_PATH}")"
 fi
-LIBPYTHON_NAME="$(basename "${LIBRARY_PATH}")"
-PYTHON_SUFFIX="$(echo "${LIBPYTHON_NAME}" | sed -r 's/^libpython(.*)\.so.*$/\1/')"
-PYTHON_INCLUDE_DIR="$(dirname "${LIBRARY_DIR}")/include/python$PYTHON_SUFFIX"
+if test "$BUILD_ZPYTHON" -eq 1 ; then
+	LIBPYTHON_NAME="$(basename "${LIBRARY_PATH}")"
+	PYTHON_SUFFIX="$(echo "${LIBPYTHON_NAME}" | sed -r 's/^libpython(.*)\.so.*$/\1/')"
+	PYTHON_INCLUDE_DIR="$(dirname "${LIBRARY_DIR}")/include/python$PYTHON_SUFFIX"
 
-export LD_LIBRARY_PATH="${LIBRARY_DIR}${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
+	export LD_LIBRARY_PATH="${LIBRARY_DIR}${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
 
-cmake .. -DZSH_REPOSITORY="$ZSH_BDIR" \
-         -DPYTHON_LIBRARY="$LIBRARY_PATH" \
-         -DPYTHON_INCLUDE_DIR="${PYTHON_INCLUDE_DIR}" \
-         -DCMAKE_INSTALL_PREFIX="$ZSH_OPT"
-make
-ldd libzpython.so
-ZTST_verbose=1 ctest -VV
-make install
+	cmake .. -DZSH_REPOSITORY="$ZSH_BDIR" \
+	         -DPYTHON_LIBRARY="$LIBRARY_PATH" \
+	         -DPYTHON_INCLUDE_DIR="${PYTHON_INCLUDE_DIR}" \
+	         -DCMAKE_INSTALL_PREFIX="$ZSH_OPT"
+	make
+	ldd libzpython.so
+	ZTST_verbose=1 ctest -VV
+	make install
+	save_exe libzpython.so
+
+	print_zpython_version() {
+		"$ZSH_OPT/bin/zsh" -c 'zmodload libzpython ; zpython "import sys; print(sys.version)"' \
+			| indent
+	}
+else
+	print_zpython_version() {
+		echo 'Python not found, zpython not built' | indent
+	}
+fi
 
 save_exe "$ZSH_OPT/bin/zsh"
-save_exe libzpython.so
 
 commit_opt_archive "$ZSH_OPT" "$ZSH_TGT" \
 "Update zsh and zpython for $LIBPYTHON_NAME
@@ -80,5 +93,5 @@ $("$ZSH_OPT/bin/zsh" --version | indent)
 
 python version:
 
-$("$ZSH_OPT/bin/zsh" -c 'zmodload libzpython; zpython "import sys; print(sys.version)"' | indent)
+$(print_zpython_version)
 $COMMIT_MESSAGE_FOOTER"
